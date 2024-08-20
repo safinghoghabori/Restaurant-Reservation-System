b1e20adc138c43e8a654f817c01c30f02fdb1d5b
SHRAVANREDDY123-itb <19r21a1272@mlrinstitutions.ac.in>
2024-08-20 15:50:41 +0530

    udated

--- Resturant.cs
+++ Resturant.cs
@@ -2,4 +2,16 @@ public class Restaurant
 {
     public string Name { get; set; }
     public List<Table> Tables { get; set; }
+
+    public Restaurant()
+    {
+        Tables = new List<Table>();
+    }
+
+    public void AddTable(Table table)
+    {
+        Tables.Add(table);
+    }
 }
+
+
--- Table.cs
+++ Table.cs
@@ -2,5 +2,6 @@ public class Table
 {
     public int TableId { get; set; }
     public int Capacity { get; set; }
+    public int cost{get;set;}
     public bool IsReserved { get; set; }
 }
