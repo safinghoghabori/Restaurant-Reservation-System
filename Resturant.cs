bb37f2f06a1e1386b2d5e32e3084e563b21af7fd
SHRAVANREDDY123-itb <19r21a1272@mlrinstitutions.ac.in>
2024-08-20 16:28:45 +0530

    changes

--- Resturant.cs
+++ Resturant.cs
@@ -2,16 +2,31 @@ public class Restaurant
 {
     public string Name { get; set; }
     public List<Table> Tables { get; set; }
+    public List<Customer> Customers{get;set;}
+    public List<Reservation>Reservations{get;set;}
 
     public Restaurant()
     {
         Tables = new List<Table>();
+        Customers = new List<Customer>();
+        Reservations = new List<Reservation>();
+
     }
 
     public void AddTable(Table table)
     {
         Tables.Add(table);
     }
+     public void AddCustomer(Customer customer)
+    {
+        Customers.Add(customer);
+    }
+
+    public void AddReservation(Reservation reservation)
+    {
+        Reservations.Add(reservation);
+    }
 }
+
 
 
