a50f2dff15b2f57998c834cf180ae5d17149a3ae
safinghoghabori <safinghoghabori65@gmail.com>
2024-08-21 11:45:04 +0530

    add feature to add tables and display them

--- Program.cs
+++ Program.cs
@@ -1,1 +1,51 @@
-﻿
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Select your role: 1. Restaurant Owner 2. Customer -> ");
+        int role = Convert.ToInt32(Console.ReadLine());
+
+        if(role == 1) 
+        {
+            Console.Write("Enter restaurant name: ");
+            string restaurantName = Console.ReadLine();
+            Restaurant restaurant = new Restaurant();
+
+            
+            while(true) {
+                Console.WriteLine("Select option you want to do: ");
+                Console.WriteLine("1. Add tables");
+                Console.WriteLine("2. Display all tables");
+                Console.Write("10. Exit ------- ");
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                if(option == 10) 
+                {
+                    break;
+                }
+
+                switch(option) {
+                    case 1: 
+                        Console.Write("Enter table id = ");
+                        int tableId = Convert.ToInt32(Console.ReadLine());    
+                        Console.Write("capacity = ");
+                        int capacity = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("cost = ");
+                        int cost = Convert.ToInt32(Console.ReadLine());
+                        restaurant.AddTable(new Table() {TableId = tableId, Capacity = capacity, Cost = cost, IsReserved = false});
+                        break;
+                    case 2: 
+                        restaurant.DisplayTables();
+                        break;
+                    default:
+                        Console.WriteLine("Kindly choose the correct option.");
+                        break;
+                }
+            }
+        }
+        else 
+        {
+            
+        }
+    }
+}
--- Resturant.cs
+++ Resturant.cs
@@ -2,26 +2,40 @@ public class Restaurant
 {
     public string Name { get; set; }
     public List<Table> Tables { get; set; }
-    public List<Customer> Customers{get;set;}
-    public List<Reservation>Reservations{get;set;}
+    public List<Customer> Customers {get;set;}
+    public List<Reservation> Reservations {get;set;}
+
+    public static int NumberOfTables = 0;
 
     public Restaurant()
     {
         Tables = new List<Table>();
         Customers = new List<Customer>();
         Reservations = new List<Reservation>();
-
     }
 
     public void AddTable(Table table)
     {
         Tables.Add(table);
     }
-     public void AddCustomer(Customer customer)
+
+    public void DisplayTables() {
+        if(Tables.Count == 0) {
+            Console.WriteLine("0 tables.");
+            return;
+        }
+
+        Console.WriteLine("====Display Tables====");
+        foreach(Table table in Tables) {
+            Console.WriteLine(table);
+        }
+        Console.WriteLine();
+    }
+
+    public void AddCustomer(Customer customer)
     {
         Customers.Add(customer);
     }
-
     public void AddReservation(Reservation reservation)
     {
         Reservations.Add(reservation);
