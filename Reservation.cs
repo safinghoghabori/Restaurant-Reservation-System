92139c7c34e6d33358d4272a8869064968b8e1af
safinghoghabori <safinghoghabori65@gmail.com>
2024-08-21 11:43:22 +0530

    change naming convention

--- Reservation.cs
+++ Reservation.cs
@@ -1,6 +1,6 @@
 public class Reservation{
-    public int Reservationid{get;set;}
-    public DateTime Datetime{get;set;}
+    public int ReservationId{get;set;}
+    public DateTime DateTime{get;set;}
     public Customer Customer{get;set;}
     public Table Table{get;set;}
 }
